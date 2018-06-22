fa7913e51f204603b984bcbc648a92b40b4ac116
benrau87 <benrau87@users.noreply.github.com>
2018-06-22 15:53:22 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,39 +1,49 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+#Prereqs for Hive and Cortex, not needed for Yeti
 #Java
-sudo add-apt-repository ppa:openjdk-r/ppa
+add-apt-repository ppa:openjdk-r/ppa
 #Elastic
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key D88E42B4
-echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
-#Hive
-echo 'deb https://dl.bintray.com/cert-bdf/debian any main' | sudo tee -a /etc/apt/sources.list.d/thehive-project.list
-sudo apt-key adv --keyserver hkp://pgp.mit.edu --recv-key 562CBC1C
-#Cortex
-echo 'deb https://dl.bintray.com/cert-bdf/debian any main' | sudo tee -a /etc/apt/sources.list.d/thehive-project.list
-sudo apt-key adv --keyserver hkp://pgp.mit.edu --recv-key 562CBC1C
-#Install
-sudo apt-get update
-sudo apt-get install apt-transport-https git wget openjdk-8-jre-headless elasticsearch thehive cortex -y
-sudo apt-get install -y --no-install-recommends python-pip python2.7-dev python3-pip python3-dev ssdeep libfuzzy-dev libfuzzy2 libimage-exiftool-perl libmagic1 build-essential git libssl-dev
-cd /etc/cortex/
-git clone https://github.com/TheHive-Project/Cortex-Analyzers
-for I in /etc/cortex/Cortex-Analyzers/analyzers/*/requirements.txt; do sudo -H pip2 install -r $I; done && \
-for I in /etc/cortex/Cortex-Analyzers/analyzers/*/requirements.txt; do sudo -H pip3 install -r $I || true; done
-
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key D88E42B4
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" |  tee -a /etc/apt/sources.list.d/elastic-5.x.list
+apt-get update
+apt-get install -y --no-install-recommends apt-transport-https openjdk-8-jre-headless elasticsearch python-pip python2.7-dev python3-pip python3-dev ssdeep libfuzzy-dev libfuzzy2 libimage-exiftool-perl libmagic1 build-essential git libssl-dev
 echo 'network.host: 127.0.0.1' > /etc/elasticsearch/elasticsearch.yml
 echo 'script.inline: on' >> /etc/elasticsearch/elasticsearch.yml
 echo 'cluster.name: hive' >> /etc/elasticsearch/elasticsearch.yml
 echo 'thread_pool.index.queue_size: 100000' >> /etc/elasticsearch/elasticsearch.yml
 echo 'thread_pool.search.queue_size: 100000' >> /etc/elasticsearch/elasticsearch.yml
 echo 'thread_pool.bulk.queue_size: 100000' >> /etc/elasticsearch/elasticsearch.yml
+systemctl enable elasticsearch
 
-sudo systemctl enable elasticsearch
-sudo systemctl enable thehive
-sudo systemctl enable cortex
+####Add or remove sections as needed if you do not want a full install
+#Hive
+echo 'deb https://dl.bintray.com/cert-bdf/debian any main' |  tee -a /etc/apt/sources.list.d/thehive-project.list
+apt-key adv --keyserver hkp://pgp.mit.edu --recv-key 562CBC1C
+apt-get update
+apt-get install -y --no-install-recommends thehive
+systemctl enable thehive
 
+#Cortex
+echo 'deb https://dl.bintray.com/cert-bdf/debian any main' |  tee -a /etc/apt/sources.list.d/thehive-project.list
+apt-key adv --keyserver hkp://pgp.mit.edu --recv-key 562CBC1C
+apt-get update
+apt-get install -y --no-install-recommends cortex
+cd /etc/cortex/
+git clone https://github.com/TheHive-Project/Cortex-Analyzers
+for I in /etc/cortex/Cortex-Analyzers/analyzers/*/requirements.txt; do  -H pip2 install -r $I; done && \
+for I in /etc/cortex/Cortex-Analyzers/analyzers/*/requirements.txt; do  -H pip3 install -r $I || true; done
+ systemctl enable cortex
+
+#Yeti
 apt-get -y install build-essential git python-dev mongodb redis-server libxml2-dev libxslt-dev zlib1g-dev python-virtualenv wkhtmltopdf 
 pip install setuptools wheel 
-curl https://raw.githubusercontent.com/yeti-platform/yeti/master/extras/ubuntu_bootstrap.sh | sudo /bin/bash
-reboot
+curl https://raw.githubusercontent.com/yeti-platform/yeti/master/extras/ubuntu_bootstrap.sh |  /bin/bash
+
+
 
 #After reboot
 #/etc/thehive/conf
