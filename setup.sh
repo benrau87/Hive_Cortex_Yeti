42910f8ae8d4f27ef0e99694e8f6309859311a8b
benrau87 <benrau87@users.noreply.github.com>
2019-06-17 15:26:30 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -40,10 +40,10 @@ _EOF_
 systemctl enable thehive
 
 #Cortex
-echo 'deb https://dl.bintray.com/cert-bdf/debian any main' |  tee -a /etc/apt/sources.list.d/thehive-project.list
-apt-key adv --keyserver hkp://pgp.mit.edu --recv-key 562CBC1C
-apt-get update
-apt-get install -y --allow-unauthenticated cortex
+echo 'deb https://dl.bintray.com/thehive-project/debian-stable any main' | sudo tee -a /etc/apt/sources.list.d/thehive-project.list
+sudo apt-key adv --keyserver hkp://pgp.mit.edu --recv-key 562CBC1C
+sudo apt-get update
+sudo apt-get install cortex
 
 (cat << _EOF_
 # Secret key
